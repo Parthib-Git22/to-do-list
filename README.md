f4535f6218ee47d3a6c2fb4dedbd69871a7b0c5c
Parthib-Git22 <parthibpaul06@gmail.com>
2024-12-08 09:07:23 +0530

    adding initial files

---
+++ README.md
@@ -0,0 +1,3 @@
+# To-Do List
+
+# A simple To-Do List app built with HTML, CSS, and JavaScript to add, edit, and remove tasks with an interactive user interface.
---
+++ app.js
@@ -0,0 +1,43 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        // there is a cross symbol at the end of each task in to-do list
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    // whenever we add any task the data get saved
+    saveData();
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+// function to save the to-do list data so that it does not go away on refreshing the browser page
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+// to display the data whenever we open the browser again
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
